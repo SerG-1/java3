cfe502d4dd618a16003e920318e52c237806f940
Serg-1 <serg52rus@ya.ru>
2020-09-27 14:05:33 +0300

    2

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -3,33 +3,41 @@ package lesson2;
 public class Main {
     public static void main(String[] args) {
 
-        int mass[] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-
-        for(int i = 0; i < 10; i++) {
-            if (mass[i] == 0) {
-                mass[i] = 1;
-            } else if (mass[i] == 1) {
-                mass[i] = 0;
-            }
-            System.out.print(mass[i] + " ");
-        }
-
+        mass();
         System.out.println();
+        mass1();
+        System.out.println();
+        mass2();
+    }
+        public static void mass(){
+            int a[] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+            for (int i = 0; i < 10; i++) {
+                if (a[i] == 0) {
+                    a[i] = 1;
+                } else if (a[i] == 1) {
+                    a[i] = 0;
+                }
+                System.out.print(a[i] + " ");
+            }
 
-        int mass1[] = new int[8];
-
-        for(int j = 0; j < 8; j++) {
-
-            mass1[j]=j*3;
-            System.out.print(mass1[j] + " ");
         }
 
-        }
+        public static void mass1() {
+            int a[] = new int[8];
 
+            for(int i = 0; i < 8; i++) {
 
+                a[i]=i*3;
+                System.out.print(a[i] + " ");
+            }
+
+    }
+        public static void mass2() {
 
 
     }
+
+}
 
 
 
