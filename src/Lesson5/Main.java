aac14a1b008524784c4100c5ae29dd08b1bbfbe8
Serejka <serg52rus@ya.ru>
2020-10-09 15:30:36 +0300

    5

--- src/Lesson5/Main.java
+++ src/Lesson5/Main.java
@@ -5,16 +5,18 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Sotrudnik Sotrudnik1 = new Sotrudnik("Akakiy Akakiy Akakiyevich", "manager", "akakiy@ya.ru",+70000000 ,13000, 40);
+        Sotrudnik Sotrudnik1 = new Sotrudnik("Akakiyev Akakiy Akakiyevich", "manager", "akakiy@ya.ru",88000000 ,13000, 40);
 
        Sotrudnik1.info();
-//        cat2.info();
 
 
-//        Cat[] cats = new Cat[3];
-//        cats[0] = new Cat("Barsik", "white", 3);
-//        cats[1] = new Cat("Barsik2", "yellow", 5);
-//        cats[2] = new Cat("Barsik3", "red", 2);
+
+          Sotrudnik[] Sotrudnik = new Sotrudnik[4];
+          Sotrudnik[0] = new Sotrudnik("Volkov Alfred Borisovich", "teamlider", "volkov@ya.ru", 88000000,30000,40);
+          Sotrudnik[1] = new Sotrudnik("Nikiforov Kirill Georgievich", "manager", "nikiforov@ya.ru", 88100000,25000,35);
+          Sotrudnik[2] = new Sotrudnik("Gerasimov Lyubomir Frolovich", "konstruktor", "gerasimov@ya.ru", 88200000,39000, 40);
+          Sotrudnik[3] = new Sotrudnik("Suvorov Ivan Alvianovich", "ingeneer", "suvorov@ya.ru", 88300000,28000,32);
+          Sotrudnik[4] = new Sotrudnik("Terentev Guriy Anatolievich", "direktor", "terentev@ya.ru", 88400000,1000000,20);
 
 //        Cat[] cats = {
 //                new Cat("Barsik", "white", 3),
