d83c24b3c8c233aa9d47055e17e5a9f49b2e2b02
Serejka <serg52rus@ya.ru>
2020-11-13 18:00:01 +0300

    9

--- src/Lesson9/HW1/Main.java
+++ src/Lesson9/HW1/Main.java
@@ -1,7 +1,32 @@
 package Lesson9.HW1;
 
-public class Main {
-    public static void main(String[] args){
+import java.util.*;
 
-    }
-}
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String[] ar = {"Декламатор", "Индейский", "Крикнуть", "Папа", "Пройти", "Папа","Пройти","Пройти","Просачиваться","Индейский"};
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s : ar) {
+            if ((map.get(s)) == null) {
+                System.out.print(s+" ");
+                map.put(s, 1);
+            } else {
+                map.put(s, (map.get(s) + 1));
+            }
+        }
+        System.out.print("\n");
+        System.out.println(map);
+    }}
+
+
+
+
+
+
+
+
+
