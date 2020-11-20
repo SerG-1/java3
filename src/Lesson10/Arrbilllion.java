51aabd633a2454db7c4919b62827cd4763dfa95a
Serejka <serg52rus@ya.ru>
2020-11-20 18:40:21 +0300

    11

---
+++ src/Lesson10/Arrbilllion.java
@@ -0,0 +1,20 @@
+package Lesson10;
+
+import java.sql.SQLOutput;
+
+public class Arrbilllion implements Runnable{
+    private String name;
+
+    public Arrbilllion(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+    }
+}
+
